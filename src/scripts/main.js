903117549279c5e62a5b57579218267c5eacb9c0
Artem Mayboroda <artmay77777@gmail.com>
2026-08-24 15:40:28 +0300

    Calculate population statistics

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const populationElements = document.querySelectorAll('.population');
+
+const populationTexts = [...populationElements].map(
+  (element) => element.textContent,
+);
+
+const populations = populationTexts.map((text) => {
+  return Number(text.replaceAll(',', ''));
+});
+
+const totalPopulation = populations.reduce(
+  (sum, population) => sum + population,
+  0,
+);
+
+const averagePopulation = totalPopulation / populations.length;
+
+const totalElement = document.querySelector('.total-population');
+const averageElement = document.querySelector('.average-population');
+
+totalElement.textContent = totalPopulation.toLocaleString('en-US');
+averageElement.textContent = averagePopulation.toLocaleString('en-US');
