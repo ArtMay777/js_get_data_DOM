6f1c1656785fe8694149bc1f2efad735c346a60e
Artem Mayboroda <artmay77777@gmail.com>
2026-08-24 17:14:06 +0300

    Validate population values

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,16 +6,20 @@ const populationTexts = [...populationElements].map(
   (element) => element.textContent,
 );
 
-const populations = populationTexts.map((text) => {
-  return Number(text.replaceAll(',', ''));
-});
+const populations = populationTexts
+  .map((text) => text.replaceAll(',', '').trim())
+  .filter((text) => text !== '')
+  .map(Number)
+  .filter(Number.isFinite);
 
 const totalPopulation = populations.reduce(
   (sum, population) => sum + population,
   0,
 );
 
-const averagePopulation = totalPopulation / populations.length;
+const averagePopulation = populations.length
+  ? totalPopulation / populations.length
+  : 0;
 
 const totalElement = document.querySelector('.total-population');
 const averageElement = document.querySelector('.average-population');
